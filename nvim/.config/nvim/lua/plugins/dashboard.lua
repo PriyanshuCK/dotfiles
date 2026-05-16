478f369a37fa25aeb39ed5d4c15244f655ae6817
Priyanshu Sharma <84620013+PriyanshuCK@users.noreply.github.com>
2026-05-16 13:40:36 +0530

    Customize NeoVim snacks dashboard

---
+++ nvim/.config/nvim/lua/plugins/dashboard.lua
@@ -0,0 +1,13 @@
+return {
+  "folke/snacks.nvim",
+  opts = {
+    dashboard = {
+      preset = {
+        header = "NeoVim",
+      },
+      sections = {
+        { section = "header" },
+      },
+    },
+  },
+}
